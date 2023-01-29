b922d73bb5b0d67b90bdf1c713f04521ffa62719
unknown <kavin816@naver.com>
2023-01-30 01:39:01 +0900

    230130

--- pjt/MVTI/자료수집_프로그램/community/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-
-urlpatterns = [
-    path('review/', views.review_list_create),
-    path('review/<int:review_pk>/', views.review_update_delete),
-]
--- pjt/MVTI/자료수집_프로그램/movies/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Movie
-
-
-class MovieSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Movie
-        fields = ('title','release_date','poster_path','overview','vote_average','original_title',)
